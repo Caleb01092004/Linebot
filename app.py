7edb20053b0a55a6557aa9d6a692ae71caaea3bc
Caleb01092004 <jaymi1975@gmail.com>
2025-05-24 18:06:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ line_handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 def sendDataTobackend(user_id):
     try:
         response = requests.post(
-            "https://linebot-wpp0.onrender.com/api/user/add",
+            "https://linebot-wpp0.onrender.com",
             #"https://digital-art-backend-nq89.onrender.com/api/user/add",
             json={"user_id": user_id}
         )
