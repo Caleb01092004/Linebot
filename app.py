b7916250dbc124e2967f22e826a111ae62765ee2
Caleb01092004 <jaymi1975@gmail.com>
2025-05-24 18:13:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,11 +100,9 @@ def liff_data():
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         line_handler.handle(body, signature)
