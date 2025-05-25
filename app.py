6dc5a359daada8a654bbdf977c93a09829b41eb7
Caleb01092004 <jaymi1975@gmail.com>
2025-05-26 03:46:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -501,4 +501,6 @@ def handle_message(event):
             )
 create_rich_menu_1()  
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render 預設給 PORT
+    app.run(host="0.0.0.0", port=port)
